22273124344f628ca2833fb43dbf283c8f531aa2
Sebastian Mottschall <srophkor@gmail.com>
2023-01-28 23:20:35 +0100

    logo added, settings moved to own menu, initializing settings with default settings, basic layout improvements

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import GameManager from "./game.js";
+import { DEFAULT_SETTINGS } from "./game.js";
 
 // Get control buttons
 const btnStart = document.querySelector('#btn-start-game');
@@ -8,24 +9,76 @@ const btnMove2 = document.querySelector('#btn-move-two');
 const btnMove3 = document.querySelector('#btn-move-three');
 const turnLeft = document.querySelector('#btn-turn-left');
 const turnRight = document.querySelector('#btn-turn-right');
+const btnUnknown = document.querySelector('#btn-unknown');
 
-// Get inputs for settings
+// Get elements for settings
 const inputRows = document.getElementById('input-rows');
 const inputColumns = document.getElementById('input-columns');
 const inputTilesize = document.getElementById('input-tile-size');
-const inputRandomMap = document.getElementById('input-random-map')
+const inputRandomMap = document.getElementById('input-random-map');
+const btnOpenSettings = document.querySelector('#navbar button.settings');
+const dlgSettings = document.getElementById('dlg-settings');
+const btnCloseSettingsWithoutSaving = document.getElementById('btn-cancel-settings');
+const btnCloseSettingsWithSaving = document.getElementById('btn-save-settings');
+const root = document.querySelector(':root');
+
+let currentBoard = null;
+
+btnStart.addEventListener('click', startGame);
+btnOpenSettings.addEventListener('click', showSettings);
+btnCloseSettingsWithoutSaving.addEventListener('click', closeSettingsWithoutSaving);
+btnCloseSettingsWithSaving.addEventListener('click', saveSettings);
+
+const settings = {
+    rows: DEFAULT_SETTINGS.rows,
+    columns: DEFAULT_SETTINGS.columns,
+    tileSize: DEFAULT_SETTINGS.tileSize,
+    randomMap: DEFAULT_SETTINGS.randomMap,
+}
+
+// to initialize the setting inputs with default values, we set the inputs once on load with values from settings
+initializeInputsFromSettings();
+
+// dev tool to export a map to save as possible static map (with some editing)
+btnUnknown.addEventListener('click', ()=>{{
+    const blob = new Blob([JSON.stringify(currentBoard)], {type: 'application/json'});
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'robotracingmap.json';
+    a.click();
+    
+}})
 
 function startGame() {
-    const settings = {};
+    const gm = new GameManager(settings);
+    currentBoard = gm.run();
+}
+
+function showSettings() {
+    dlgSettings.showModal();
+}
+
+function initializeInputsFromSettings() {
+    inputRows.value = settings.rows;
+    inputColumns.value = settings.columns;
+    inputTilesize.value = settings.tileSize;
+    inputRandomMap.checked = settings.randomMap;
+    root.style.setProperty('--tile-size', `${settings.tileSize}px`);
+}
+
+function closeSettingsWithoutSaving() {
+    initializeInputsFromSettings();
+    dlgSettings.close();
+}
+
+function saveSettings() {
     settings.rows = Number(inputRows.value);
     settings.columns = Number(inputColumns.value);
     settings.tileSize = Number(inputTilesize.value);
     settings.randomMap = inputRandomMap.checked;
-    console.log('settings', settings)
+    dlgSettings.close();
 
-    const gm = new GameManager(settings);
-    gm.run();
-    
+    // randomMap, rows and columns are only used on starting a new game
+    // but tile size is immediately applied, so we can resize an existing game board
+    root.style.setProperty('--tile-size', `${settings.tileSize}px`);
 }
-
-btnStart.addEventListener('click', startGame);
